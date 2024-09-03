6b46d652766a024cdc1e4787e58b7d5879eb0a4f
L-Amos <luke.amos77@outlook.com>
2024-09-03 22:18:00 +0100

    Sorting projects and project buttons.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,6 +6,10 @@ export default {
 			fontFamily: {
 				heading: ["Bebas Neue", "sans-serif"],
 				body: ["Inter Tight", "sans-serif"],
+			},
+			boxShadow: {
+				button: '-10px 5px 0px rgba(75, 85, 99, 1)',
+				button_hover: '-15px 8px 0px rgba(74, 222, 128, 1)'
 			}
 		},
 	},
