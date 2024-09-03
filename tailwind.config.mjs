55fb5c136307abb1629d7d6445ec4bc42064ce43
L-Amos <luke.amos77@outlook.com>
2024-09-03 20:25:56 +0100

    Adding fonts.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,12 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				heading: ["Bebas Neue", "sans-serif"],
+				body: ["Inter Tight", "sans-serif"],
+			}
+		},
 	},
 	plugins: [],
 }
